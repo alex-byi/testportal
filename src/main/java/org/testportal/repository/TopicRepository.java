ef947afb936a7c98d0de172fccf7204be4345d90
alex-byi <alexeybyimistrov@gmail.com>
2020-06-22 20:51:17 +0300

    oauth2 authorization

--- src/main/java/org/testportal/repository/TopicRepository.java
+++ src/main/java/org/testportal/repository/TopicRepository.java
@@ -3,7 +3,6 @@ package org.testportal.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.testportal.entity.Topic;
 
-import java.util.List;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
 }
